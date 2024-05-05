0d02333eb4e4f2c920961cc5baa46488bfbe8901
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-05-06 00:07:43 +0200

    setup navigation routes

---
+++ frontend/src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeScreen />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
